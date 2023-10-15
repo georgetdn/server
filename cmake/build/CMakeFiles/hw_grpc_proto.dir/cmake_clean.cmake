4d592432e10cb329b7093084dceb5b7a9fca94be
georgetdn <gdobrescu@tdnsys.com>
2023-10-15 21:32:42 +0000

    Initial commit

---
+++ cmake/build/CMakeFiles/hw_grpc_proto.dir/cmake_clean.cmake
@@ -0,0 +1,15 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/hw_grpc_proto.dir/prototype.grpc.pb.cc.o"
+  "CMakeFiles/hw_grpc_proto.dir/prototype.pb.cc.o"
+  "libhw_grpc_proto.a"
+  "libhw_grpc_proto.pdb"
+  "prototype.grpc.pb.cc"
+  "prototype.grpc.pb.h"
+  "prototype.pb.cc"
+  "prototype.pb.h"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/hw_grpc_proto.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ cmake/build/CMakeFiles/server.dir/DependInfo.cmake
@@ -0,0 +1,31 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/home/georged/grpc/examples/cpp/prototype/dbtrans.cc" "/home/georged/grpc/examples/cpp/prototype/cmake/build/CMakeFiles/server.dir/dbtrans.cc.o"
+  "/home/georged/grpc/examples/cpp/prototype/repository.cc" "/home/georged/grpc/examples/cpp/prototype/cmake/build/CMakeFiles/server.dir/repository.cc.o"
+  "/home/georged/grpc/examples/cpp/prototype/server.cc" "/home/georged/grpc/examples/cpp/prototype/cmake/build/CMakeFiles/server.dir/server.cc.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# Preprocessor definitions for this target.
+set(CMAKE_TARGET_DEFINITIONS_CXX
+  "CARES_STATICLIB"
+  )
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "."
+  "/usr/include/mysql"
+  "/include"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  "/home/georged/grpc/examples/cpp/prototype/cmake/build/CMakeFiles/hw_grpc_proto.dir/DependInfo.cmake"
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
